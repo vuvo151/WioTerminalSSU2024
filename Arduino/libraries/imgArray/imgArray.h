7436222f6822aed71463af4b5f59e927917e6910
Ngoc Vu Vo <vuvo151@gmail.com>
2024-03-14 18:36:46 +0900

    Add library for Display Image
    Add library for Display Image

---
+++ Arduino/libraries/imgArray/imgArray.h
@@ -0,0 +1,11 @@
+#ifndef IMGARRAY_H
+#define IMGARRAY_H
+#include <stdint.h>
+
+extern const int imsArr[]; // Declare imsArr array
+
+extern const uint32_t ssuLogo[]; // Declare imsArr array
+
+#endif
+
+
